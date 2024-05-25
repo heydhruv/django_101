03820915c858e087d72ba5bcbd19c934b0bc5751
Dhruv Dave <heydhruvdave@gmail.com>
2024-05-25 23:32:11 +0530

    renamed

--- django_101/urls.py
+++ django_101/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for wanbuffer project.
+URL configuration for django_101 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
